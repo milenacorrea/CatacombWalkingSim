1af142e5565c869bb30d48553fffa3c44f722ae8
Chang <hahnc589@newschool.edu>
2020-02-06 11:09:12 -0500

    changs build

---
+++ WalkingSimulationProject/Assets/DoorOpen.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorOpen : MonoBehaviour
+{
+    private Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player") {
+            animator.SetTrigger("Open");
+            Debug.Log("dd");
+        }
+    }
+}
